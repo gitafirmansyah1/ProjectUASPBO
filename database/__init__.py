b1002c4864182fdf3ce16f508a86a8c41b67a1c7
Winda Nengsi <windanengsi5769@gmail.com>
2026-07-02 09:25:16 +0700

    Menambahkan package initialization database

---
+++ database/__init__.py
@@ -0,0 +1,10 @@
+"""
+Paket database untuk Sistem Validasi RPS-BAP.
+
+Paket ini menyediakan class DatabaseConnection untuk
+mengelola koneksi dan operasi database MySQL.
+"""
+
+from database.connection import DatabaseConnection
+
+__all__ = ["DatabaseConnection"]
